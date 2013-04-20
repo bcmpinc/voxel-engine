2edc8e7804469b773c9ca7b530280709cc444fd7
Bauke Conijn <bauke.conijn@quintiq.com>
2013-04-20 13:28:39 +0200

    Improved windows run script

--- run.bat
+++ run.bat
@@ -1,12 +1,31 @@
 @echo off
 SET PATH=C:\mingw64\bin;C:\mingw64\x86_64-w64-mingw32\bin;C:\Program Files (x86)\CMake 2.8\bin;%PATH%
-make -j 8 || goto fail
 
+:ask
+CHOICE /C VCHQ /M "[V]ox, [C]onvert, [H]eightmap [Q]uit"
+IF ERRORLEVEL 4 GOTO quit:
+IF ERRORLEVEL 3 GOTO heightmap:
+IF ERRORLEVEL 2 GOTO convert:
+IF ERRORLEVEL 1 GOTO vox:
+GOTO ask
+
+:vox
+make -j 8 vox || goto ask
 vox
+goto ask
 
-goto end
+:convert
+make -j 8 convert || goto ask
+if not exist points.vxl (
+  convert points
+) else (
+  echo File already exists.
+)
+goto ask
 
-:fail
-pause
+:heightmap
+make -j 8 heightmap || goto ask
+heightmap
+goto ask
 
-:end
+:quit
