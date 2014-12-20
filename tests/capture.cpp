c64a585802cd85bc14e486c0de7dbe0eeb901b88
Bauke Conijn <bcmpinc@users.sourceforge.net>
2014-12-20 16:31:25 +0100

    Removed makefile

--- tests/capture.cpp
+++
@@ -1,8 +0,0 @@
-#define __STDC_CONSTANT_MACROS
-extern "C" { 
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavutil/avutil.h>
-#include <libswscale/swscale.h>
-}
-int main(){return 0;}
