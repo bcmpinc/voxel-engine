aca46e5bed5855f2d20fee08f63d46e2f55d789c
Bauke Conijn <bcmpinc@users.sourceforge.net>
2015-03-23 02:50:55 +0100

    Licenses

--- src/engine/octree_draw.cpp
+++ src/engine/octree_draw.cpp
@@ -1,6 +1,6 @@
 /*
     Voxel-Engine - A CPU based sparse octree renderer.
-    Copyright (C) 2013,2014  B.J. Conijn <bcmpinc@users.sourceforge.net>
+    Copyright (C) 2013,2014,2015  B.J. Conijn <bcmpinc@users.sourceforge.net>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
