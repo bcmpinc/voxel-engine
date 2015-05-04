e6d85ab058f0b647f28d7ffe9ee13c7f27de2daa
Bauke Conijn <bcmpinc@users.sourceforge.net>
2015-05-04 17:22:09 +0200

    Added 0-hole scanner

--- src/holes.cpp
+++ src/holes.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
 
+/** Scans for 0-holes in files. */
 int main(int argc, char ** argv) {
     if (argc != 2) {
         printf("Usage: %s filename\n", argv[0]);
@@ -11,8 +13,22 @@ int main(int argc, char ** argv) {
         printf("Failed to open '%s'\n", argv[1]);
         exit(1);
     }
-    while(!feof(f)) {
-        
+    int buffer;
+    int start = 0;
+    int pos = 0;
+    bool empty=false;
+    while(fread(&buffer, sizeof(buffer), 1, f)) {
+        if (empty != (buffer==0)) {
+            if (pos - start >= 8) {
+                printf("%s segment from %d to %d.\n", empty?"null":"data", start, pos);
+            }
+            empty = (buffer==0);
+            start = pos;
+        }
+        pos+=4;
+    }
+    if (pos - start >= 8) {
+        printf("%s segment from %d to %d.\n", empty?"null":"data", start, pos);
     }
     return 0;
 }
